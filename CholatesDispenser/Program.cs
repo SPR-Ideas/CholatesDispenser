cbb52816b5012145745e80726d0149657fd771be
pradeepragul <1606pradeepragul@gmail.com>
2023-03-27 18:39:18 +0530

    Add project files.

---
+++ CholatesDispenser/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CholatesDispenser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dispenser dis = new dispenser(20);
+            dis.addCholates("red", 10);
+            Dictionary<string, int> d = dis.dispenseCholates(5);
+
+            
+        }
+    }
+}
---
+++ CholatesDispenser/dispenser.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CholatesDispenser
+{
+    public class dispenser
+    {
+
+        //int red , green , blue ,sliver ,crimson ,purpule , pink;
+        public Dictionary<string, int> choclates = new Dictionary<string, int>();
+        public dispenser(int count) {
+            choclates["red"] = count;
+            choclates["blue"] = count;
+            choclates["green"] = count;
+            choclates["sliver"] = count;
+            choclates["crimson"] = count;
+            choclates["purpule"] = count;
+            choclates["pink"] = count;
+
+        }
+
+        public void addCholates(string color, int count)
+        {
+            choclates[color] += count;
+        }
+
+        public Dictionary<string, int> removeCholates(int count)
+        {
+            //char[] cholates = new char[count * 7];
+            foreach (KeyValuePair<string, int> kp in choclates) {
+                choclates[kp.Key] = kp.Value - count;
+            }
+
+            return choclates;
+        }
+
+        public Dictionary<string, int> dispenseCholates(int count) {
+
+            Dictionary<string, int> new_chocolate = new Dictionary<string, int>();
+
+            while (count > 0) {
+                foreach (KeyValuePair<string, int> i in choclates) {
+                    if (i.Value > 0)
+                    {
+                        choclates[i.Key] = i.Value - 1;
+                        new_chocolate[i.Key] = new_chocolate.GetValueOrDefault(i.Key, 0) + 1;
+                        count--;
+                    }
+                    if (count == 0)
+                        break;
+                }
+            }
+            return new_chocolate;
+        }
+
+        public int dispenseChocolateOfColor(int count, string color) {
+
+            choclates[color] -= count;
+            return count;
+        }
+
+        public int[] noOfCholcolate()
+        {
+            int[] result = new int[7];
+            int count = 0;
+            foreach (KeyValuePair<string, int> kp in choclates) {
+                result[count] = kp.Value;
+                count++;
+            }
+
+            return result;
+        }
+
+        public string[] sortCholcoateBasedOnCount()
+        {
+            string[] result = new string[7];
+            int count = 0;
+            foreach(KeyValuePair <string,int> i in choclates.OrderBy(x => x.Value))
+            {
+                result[count] = i.Key;
+                count++;
+            }
+            return result;  
+        }
+
+
+        public void chageChocolateColorAllxCount(int number ,string color,string finalColor ) {
+            choclates[color] -= number;
+            choclates[finalColor] += number;
+        }
+
+        public void chageChocolateColorAllOfxCount( string color, string finalColor)
+        {
+            int count = choclates[color] ;
+            choclates[color] = 0;
+            choclates[finalColor] += count;
+        }
+    
+
+    
+    }
+
+
+
+
+
+}
